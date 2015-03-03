df61526e1fb290fc07213e209c6f2872fcc197fa
Rafał Nowak <rafal.nowak1@gmail.com>
2015-03-03 21:26:53 +0100

    Clean up of main algorithm code.

--- src/main/scala/info/rnowak/simplega/algorithm/GeneticAlgorithm.scala
+++ src/main/scala/info/rnowak/simplega/algorithm/GeneticAlgorithm.scala
@@ -11,26 +11,25 @@ class GeneticAlgorithm[PopulationType <: Population] {
           minimumFitness: FitnessValue,
           maxIterations: Long)
          (fitness: FitnessFunction[PopulationType]): Stream[AlgorithmStepResult[PopulationType]] = {
-    val currentPopulation = populationContext.createInitialPopulation()
-    val algorithmSteps = populationStream(currentPopulation)(populationContext, minimumFitness, fitness, maxIterations)
-    algorithmSteps.takeWhile { step =>
+    val initialPopulation = populationContext.createInitialPopulation()
+    populationStream(initialPopulation)(populationContext, minimumFitness, fitness, maxIterations) takeWhile { step =>
       //TODO: więcej mądrych warunków stopu
       step.currentGeneration < maxIterations
     }
   }
   
   //TODO: posprzątać ten bałagan
-  private def populationStream(currentPopulation: PopulationType)
-                      (populationContext: PopulationContext[PopulationType],
-                       minimumFitness: FitnessValue,
-                       fitness: FitnessFunction[PopulationType],
-                       maxIterations: Long): Stream[AlgorithmStepResult[PopulationType]] = {
+  private def populationStream(initialPopulation: PopulationType)
+                              (populationContext: PopulationContext[PopulationType],
+                               minimumFitness: FitnessValue,
+                               fitness: FitnessFunction[PopulationType],
+                               maxIterations: Long): Stream[AlgorithmStepResult[PopulationType]] = {
     lazy val populations: Stream[AlgorithmStepResult[PopulationType]] = Stream.cons(
-      initialStep(currentPopulation, fitness), 
+      initialStep(initialPopulation, fitness),
       populations map { step =>
-        val individualsSortedByFitness = fitness.calculateFor(currentPopulation).sortBy(_.fitness.value)
+        val individualsSortedByFitness = fitness.calculateFor(initialPopulation).sortBy(_.fitness)
         val newIndividuals = for {
-          i <- 1 to currentPopulation.size
+          i <- 1 to initialPopulation.size
         } yield crossoverAndMutate(individualsSortedByFitness)(populationContext)
         val newPopulation = populationContext.createPopulationFromIndividuals(newIndividuals.flatten)
         AlgorithmStepResult[PopulationType](step.currentGeneration + 1, newPopulation, bestFitnessForPopulation(newPopulation, fitness))
@@ -42,10 +41,8 @@ class GeneticAlgorithm[PopulationType <: Population] {
   private def initialStep(population: PopulationType, fitness: FitnessFunction[PopulationType]): AlgorithmStepResult[PopulationType] = 
     AlgorithmStepResult(0, population, bestFitnessForPopulation(population, fitness))
   
-  private def bestFitnessForPopulation(population: PopulationType, fitness: FitnessFunction[PopulationType]): FitnessValue = {
-    val individualsWithFitness = fitness.calculateFor(population)
-    individualsWithFitness.map(_.fitness).min
-  }
+  private def bestFitnessForPopulation(population: PopulationType, fitness: FitnessFunction[PopulationType]): FitnessValue = 
+    fitness.calculateFor(population).map(_.fitness).min
   
   private def crossoverAndMutate(individualsSorted: Seq[IndividualWithFitness[PopulationType#IndividualType]])
                                 (context: PopulationContext[PopulationType]): Children[PopulationType] = {
