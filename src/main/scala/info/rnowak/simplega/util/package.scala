faf030c7729e50f767cac437b7c3305d0d4a26c4
Rafał Nowak <rafal.nowak1@gmail.com>
2015-04-04 14:03:46 +0200

    RNG clean up - next round.

--- src/main/scala/info/rnowak/simplega/util/package.scala
+++ src/main/scala/info/rnowak/simplega/util/package.scala
@@ -4,4 +4,7 @@ import scalaz.State
 
 package object util {
   type Rand[A] = State[RNG, A]
+
+  val randomInt: Rand[Int] = State(_.nextInt)
+  val randomDouble: Rand[Double] = State(_.nextDouble)
 }
